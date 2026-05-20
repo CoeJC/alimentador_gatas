975e64fe79bcabb0daaeb2f686e801e8bbe2202f
CoeJC <kilerbrgames@gmail.com>
2026-05-19 22:50:48 -0300

    Update trpc.ts

--- server/_core/trpc.ts
+++ server/_core/trpc.ts
@@ -10,22 +10,7 @@ const t = initTRPC.context<TrpcContext>().create({
 export const router = t.router;
 export const publicProcedure = t.procedure;
 
-const requireUser = t.middleware(async opts => {
-  const { ctx, next } = opts;
-
-  if (!ctx.user) {
-    throw new TRPCError({ code: "UNAUTHORIZED", message: UNAUTHED_ERR_MSG });
-  }
-
-  return next({
-    ctx: {
-      ...ctx,
-      user: ctx.user,
-    },
-  });
-});
-
-export const protectedProcedure = t.procedure.use(requireUser);
+export const protectedProcedure = t.procedure;
 
 export const adminProcedure = t.procedure.use(
   t.middleware(async opts => {
